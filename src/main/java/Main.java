12ef8b7d2ecc9aaa294ccff315fa0abe288cd362
tirthkumar25bcs10711 <tirthkumar.25bcs10711@sst.scaler.com>
2026-06-20 15:56:55 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,10 @@ public class Main {
             String input = scanner.nextLine();
             if (input.equals("exit")) {
                 break;
+            } else if (input.startsWith("echo ")) {
+                System.out.println(input.substring(5));
+            } else {
+                System.out.println(input + ": command not found");
             }
             System.out.println(input + ": command not found");
         }
