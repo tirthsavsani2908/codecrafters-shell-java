bdf206306b3f73cc2e5da43e9fc26184f0c739c3
tirthkumar25bcs10711 <tirthkumar.25bcs10711@sst.scaler.com>
2026-06-20 15:59:18 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -16,12 +17,35 @@ public class Main {
                 System.out.println(input.substring(5));
             } 
             else if (input.startsWith("type ")) {
+
                 String command = input.substring(5);
 
+                // Check builtins
                 if (command.equals("echo") || command.equals("exit") || command.equals("type")) {
                     System.out.println(command + " is a shell builtin");
-                } else {
-                    System.out.println(command + ": not found");
+                } 
+                else {
+                    boolean found = false;
+
+                    String path = System.getenv("PATH");
+
+                    if (path != null) {
+                        String[] directories = path.split(File.pathSeparator);
+
+                        for (String dir : directories) {
+                            File file = new File(dir, command);
+
+                            if (file.exists() && file.isExecutable()) {
+                                System.out.println(command + " is " + file.getAbsolutePath());
+                                found = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println(command + ": not found");
+                    }
                 }
             } 
             else {
