9b90cc7499ac030219beb9d935f64b3266212f57
tirthkumar25bcs10711 <tirthkumar.25bcs10711@sst.scaler.com>
2026-06-20 15:58:22 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,19 +2,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
-         
+
         Scanner scanner = new Scanner(System.in);
-         while (true) {
+
+        while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
+
             if (input.equals("exit")) {
                 break;
-            } else if (input.startsWith("echo ")) {
+            } 
+            else if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
-            } else {
+            } 
+            else if (input.startsWith("type ")) {
+                String command = input.substring(5);
+
+                if (command.equals("echo") || command.equals("exit") || command.equals("type")) {
+                    System.out.println(command + " is a shell builtin");
+                } else {
+                    System.out.println(command + ": not found");
+                }
+            } 
+            else {
                 System.out.println(input + ": command not found");
             }
         }
+
+        scanner.close();
     }
 }
